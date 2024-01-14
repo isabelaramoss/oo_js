1f249ca32021c80a5972841335a84ea7c49ce7f5
Isabela Ramos <isamrteixeira@gmail.com>
2024-01-14 13:38:15 -0300

    primeira entrega

---
+++ exercicio.js
@@ -0,0 +1,33 @@
+// Crie uma classe de uma abstração;
+// Crie pelo menos duas classes que sejam herdeiras da classe criada anteriormente;
+// Crie pelo menos três instâncias de objetos; 
+
+function Aluno(nome){
+  this.nome = nome;
+
+  this.apresenta = function(){
+    console.log(`Meu nome é ${this.nome}.`);
+  }
+}
+
+function InfosAlunos(nome, idade, materiaFavorita){
+  this.nome = nome;
+  this.idade = idade;
+  this.materiaFavorita = materiaFavorita;
+
+  Aluno.call(this, nome);
+}
+
+//Instâncias:
+const aluno1 = new InfosAlunos("Maria", "10", "Português");
+const aluno2 = new InfosAlunos("João", "9", "Artes");
+const aluno3 = new InfosAlunos("Beatriz", "10", "História");
+
+console.log(aluno1);
+aluno1.apresenta();
+
+console.log(aluno2);
+aluno2.apresenta();
+
+console.log(aluno3);
+aluno3.apresenta();
