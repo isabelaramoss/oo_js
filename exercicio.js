6c4edc5e5724b53057f7aa362c822c27d3772d0f
Isabela Ramos <isamrteixeira@gmail.com>
2024-01-14 13:48:22 -0300

    entrega

--- exercicio.js
+++ exercicio.js
@@ -1,7 +1,3 @@
-// Crie uma classe de uma abstração;
-// Crie pelo menos duas classes que sejam herdeiras da classe criada anteriormente;
-// Crie pelo menos três instâncias de objetos; 
-
 function Aluno(nome){
   this.nome = nome;
 
