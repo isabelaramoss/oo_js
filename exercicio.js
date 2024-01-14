a7ecffdb6cd65dff53cc501dce51e5cc9256f87b
Isabela Ramos <isamrteixeira@gmail.com>
2024-01-14 14:21:23 -0300

    segunda classe filha

--- exercicio.js
+++ exercicio.js
@@ -1,4 +1,4 @@
-function Aluno(nome){
+function Pessoa(nome){
   this.nome = nome;
 
   this.apresenta = function(){
@@ -11,7 +11,15 @@ function InfosAlunos(nome, idade, materiaFavorita){
   this.idade = idade;
   this.materiaFavorita = materiaFavorita;
 
-  Aluno.call(this, nome);
+  Pessoa.call(this, nome);
+}
+
+function InfosProfessores(nome, idade, materiaLecionada){
+  this.nome = nome;
+  this.idade = idade;
+  this.materiaLecionada = materiaLecionada;
+
+  Pessoa.call(this, nome);
 }
 
 //Instâncias:
@@ -27,3 +35,16 @@ aluno2.apresenta();
 
 console.log(aluno3);
 aluno3.apresenta();
+
+const professor1 = new InfosProfessores("Lívia", "40", "Geografia");
+const professor2 = new InfosProfessores("Matheus", "32", "Biologia");
+const professor3 = new InfosProfessores("Adriana", "60", "Matemática");
+
+console.log(professor1);
+professor1.apresenta();
+
+console.log(professor2);
+professor2.apresenta();
+
+console.log(professor3);
+professor3.apresenta();
